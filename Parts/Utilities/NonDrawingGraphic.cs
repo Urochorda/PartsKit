7656dd019d67417ddd6f29138f02bbbdb058644b
wangchaoqi <wangchaoqi@dxx.cn>
2024-01-19 16:23:31 +0800

    [feat]添加功能组件

---
+++ Parts/Utilities/NonDrawingGraphic.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PartsKit
+{
+    /// A concrete subclass of the Unity UI `Graphic` class that just skips drawing.
+    /// Useful for providing a raycast target without actually drawing anything.
+    [RequireComponent(typeof(CanvasRenderer))]
+    public class NonDrawingGraphic : MaskableGraphic
+    {
+        public override void SetMaterialDirty() { return; }
+        public override void SetVerticesDirty() { return; }
+
+        /// Probably not necessary since the chain of calls `Rebuild()`->`UpdateGeometry()`->`DoMeshGeneration()`->`OnPopulateMesh()` won't happen; so here really just as a fail-safe.
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+        }
+    }
+}

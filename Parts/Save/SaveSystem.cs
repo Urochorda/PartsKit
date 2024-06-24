6f8e332efd1b1e781c8953fd95f968d466b12b90
wangchaoqi <wangchaoqi@dxx.cn>
2024-06-24 20:28:40 +0800

    [feat]完善逻辑

--- Parts/Save/SaveSystem.cs
+++ Parts/Save/SaveSystem.cs
@@ -9,14 +9,24 @@ namespace PartsKit
         public static SaveSystem Global { get; } = new SaveSystem(); //默认全局存储系统
         public EventItem SaveEvent { get; } = new EventItem();
 
+        public string UserKey { get; set; } = string.Empty;
+
         public void Set<T>(string name, T data)
         {
+            if (!string.IsNullOrEmpty(UserKey))
+            {
+                name = UserKey + name;
+            }
             string dataStr = JsonConvert.SerializeObject(data);
             PlayerPrefs.SetString(name, dataStr);
         }
 
         public bool Get<T>(string name, out T data)
         {
+            if (!string.IsNullOrEmpty(UserKey))
+            {
+                name = UserKey + name;
+            }
             return Get(name, null, out data);
         }
 

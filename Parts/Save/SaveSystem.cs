814dab80edfe88ee598939796783bde2519f9bd2
wangchaoqi <wangchaoqi@dxx.cn>
2024-07-29 20:47:55 +0800

    [feat]完善存档逻辑

--- Parts/Save/SaveSystem.cs
+++ Parts/Save/SaveSystem.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
 
@@ -9,13 +10,26 @@ namespace PartsKit
         public static SaveSystem Global { get; } = new SaveSystem(); //默认全局存储系统
         public EventItem SaveEvent { get; } = new EventItem();
 
-        public string UserKey { get; set; } = string.Empty;
+        public string UserKey { get; private set; }
+
+        private List<string> userAllName = new List<string>();
+
+        public void SetUser(string user)
+        {
+            UserKey = user;
+            userAllName = GetUserAllName(user);
+        }
 
         public void Set<T>(string name, T data)
         {
             name = GetName(name);
             string dataStr = JsonConvert.SerializeObject(data);
             PlayerPrefs.SetString(name, dataStr);
+            if (HasCurUserKey() && !userAllName.Contains(name))
+            {
+                userAllName.Add(name);
+                SaveCurUserAllName();
+            }
         }
 
         public bool Get<T>(string name, out T data)
@@ -51,6 +65,20 @@ namespace PartsKit
         {
             name = GetName(name);
             PlayerPrefs.DeleteKey(name);
+            if (HasCurUserKey() && userAllName.Contains(name))
+            {
+                userAllName.Remove(name);
+                SaveCurUserAllName();
+            }
+        }
+
+        public void DeleteAllByUser(string user)
+        {
+            var targetAllName = GetUserAllName(user);
+            foreach (string name in targetAllName)
+            {
+                Delete(name);
+            }
         }
 
         public void Save()
@@ -61,12 +89,34 @@ namespace PartsKit
 
         private string GetName(string name)
         {
-            if (!string.IsNullOrEmpty(UserKey))
+            if (HasCurUserKey())
             {
                 name = $"{UserKey}.{name}";
             }
 
             return name;
+        }
+
+        private bool HasCurUserKey()
+        {
+            return !string.IsNullOrEmpty(UserKey);
+        }
+
+        private void SaveCurUserAllName()
+        {
+            string userAllNameStr = JsonConvert.SerializeObject(userAllName);
+            PlayerPrefs.SetString(GetUserAllNameSaveKey(UserKey), userAllNameStr);
+        }
+
+        private string GetUserAllNameSaveKey(string user)
+        {
+            return $"{user}_AllName";
+        }
+
+        private List<string> GetUserAllName(string user)
+        {
+            Get(GetUserAllNameSaveKey(user), () => new List<string>(), out List<string> allName);
+            return allName;
         }
     }
 }

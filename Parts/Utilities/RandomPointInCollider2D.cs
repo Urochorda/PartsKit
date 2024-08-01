4e51ab1c08757749f4208cbad44c7fcf81126e13
wangchaoqi <wangchaoqi@dxx.cn>
2024-08-01 11:05:06 +0800

    [feat]完善逻辑

--- Parts/Utilities/RandomPointInCollider2D.cs
+++ Parts/Utilities/RandomPointInCollider2D.cs
@@ -6,31 +6,13 @@ namespace PartsKit
 {
     public class RandomPointInCollider2D
     {
-        private Collider2D collider;
-        private Vector3 minBound;
-        private Vector3 maxBound;
+        private Collider2D mCollider;
         private int maxRandomCount = 1000;
         private Func<float, float, float> onRandom;
 
         public RandomPointInCollider2D(Collider2D colliderVal)
         {
-            SetCollider(colliderVal);
-        }
-
-        public void SetCollider(Collider2D colliderVal)
-        {
-            if (colliderVal == null)
-            {
-                collider = null;
-                minBound = Vector3.zero;
-                maxBound = Vector3.zero;
-                return;
-            }
-
-            collider = colliderVal;
-            var bounds = collider.bounds;
-            minBound = bounds.min;
-            maxBound = bounds.max;
+            mCollider = colliderVal;
         }
 
         public void SetMaxRandomCount(int count)
@@ -45,6 +27,9 @@ namespace PartsKit
 
         public Vector3 RandomPoint()
         {
+            var bounds = mCollider.bounds;
+            Vector3 minBound = bounds.min;
+            Vector3 maxBound = bounds.max;
             Vector3 randomPoint;
             int randomCount = 0;
             do
@@ -56,7 +41,7 @@ namespace PartsKit
                         RandomValue(minBound.z, maxBound.z)
                     );
                 randomCount++;
-            } while (!collider.OverlapPoint(randomPoint) && randomCount <= maxRandomCount);
+            } while (!mCollider.OverlapPoint(randomPoint) && randomCount <= maxRandomCount);
 
             return randomPoint;
         }
@@ -68,7 +53,7 @@ namespace PartsKit
                 return onRandom.Invoke(min, max);
             }
 
-            return Random.Range(minBound.x, maxBound.x);
+            return Random.Range(min, max);
         }
     }
 }

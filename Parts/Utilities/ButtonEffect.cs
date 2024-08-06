567cbfcfc99e30baad20fc28b4d670899c36a48d
wangchaoqi <wangchaoqi@dxx.cn>
2024-08-06 20:25:44 +0800

    [feat]按钮支持新动画

--- Parts/Utilities/ButtonEffect.cs
+++ Parts/Utilities/ButtonEffect.cs
@@ -10,7 +10,11 @@ namespace PartsKit
         public static Action<ButtonEffect> OnDownGlobal { get; set; }
         public static Action<ButtonEffect> OnUpGlobal { get; set; }
 
+        [SerializeField] private bool scaleAnim = true;
         [SerializeField] private CheckNullProperty<Transform> rootPoint;
+        [SerializeField] private bool selectAnim;
+        [SerializeField] private GameObject[] downObj;
+        [SerializeField] private GameObject[] upObj;
         private Sequence animSequence;
         private Transform SafePoint => rootPoint.GetValue(out Transform rootPointValue) ? rootPointValue : transform;
         public event Action<ButtonEffect> OnDown;
@@ -18,13 +22,27 @@ namespace PartsKit
 
         private Vector3 downScale;
 
+        private void Awake()
+        {
+            SetSelectObj(false, true);
+        }
+
         public void OnPointerDown(PointerEventData eventData)
         {
             animSequence?.Kill(); //先kill，会恢复localScale，使得下次记录的downScale正确
             downScale = SafePoint.localScale;
-            animSequence = DOTween.Sequence();
-            animSequence.Append(SafePoint.DOScale(downScale * 0.9f, 0.15f));
-            animSequence.OnKill(() => { animSequence = null; });
+            if (scaleAnim)
+            {
+                animSequence = DOTween.Sequence();
+                animSequence.Append(SafePoint.DOScale(downScale * 0.9f, 0.15f));
+                animSequence.OnKill(() => { animSequence = null; });
+            }
+
+            if (selectAnim)
+            {
+                SetSelectObj(true, false);
+            }
+
             OnDown?.Invoke(this);
             OnDownGlobal?.Invoke(this);
         }
@@ -32,20 +50,53 @@ namespace PartsKit
         public void OnPointerUp(PointerEventData eventData)
         {
             animSequence?.Kill();
-            animSequence = DOTween.Sequence();
-            animSequence.Append(SafePoint.DOScale(downScale, 0.15f));
-            animSequence.OnKill(() =>
+            if (scaleAnim)
             {
-                SafePoint.localScale = downScale;
-                animSequence = null;
-            });
+                animSequence = DOTween.Sequence();
+                animSequence.Append(SafePoint.DOScale(downScale, 0.15f));
+                animSequence.OnKill(() =>
+                {
+                    SafePoint.localScale = downScale;
+                    animSequence = null;
+                });
+            }
+
+            if (selectAnim)
+            {
+                SetSelectObj(false, true);
+            }
+
             OnUp?.Invoke(this);
             OnUpGlobal?.Invoke(this);
+        }
+
+        private void SetSelectObj(bool isDown, bool isUp)
+        {
+            foreach (var o in downObj)
+            {
+                if (o == null)
+                {
+                    continue;
+                }
+
+                o.SetActive(isDown);
+            }
+
+            foreach (var o in upObj)
+            {
+                if (o == null)
+                {
+                    continue;
+                }
+
+                o.SetActive(isUp);
+            }
         }
 
         private void OnDisable()
         {
             animSequence?.Kill();
+            SetSelectObj(false, true);
         }
     }
 }

7be799db74367c247bfe3adf264b9f22b2f87bee
wangchaoqi <wangchaoqi@dxx.cn>
2025-03-26 20:11:56 +0800

    [feat]添加工具方法

--- Basic/Utilities/Extensions.cs
+++ Basic/Utilities/Extensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace PartsKit
 {
@@ -47,6 +48,109 @@ namespace PartsKit
         {
             int index = self.IndexOf(item);
             return self.RemoveAtDisorder(index);
+        }
+
+        public static Bounds Encapsulate(this List<Bounds> boundsList)
+        {
+            float minX = float.MaxValue;
+            float maxX = float.MinValue;
+
+            float minY = float.MaxValue;
+            float maxY = float.MinValue;
+
+            float minZ = float.MaxValue;
+            float maxZ = float.MinValue;
+            foreach (var bonds in boundsList)
+            {
+                var minPos = bonds.min;
+                var maxPos = bonds.max;
+                if (minPos.x < minX)
+                {
+                    minX = minPos.x;
+                }
+
+                if (maxPos.x > maxX)
+                {
+                    maxX = maxPos.x;
+                }
+
+                if (minPos.y < minY)
+                {
+                    minY = minPos.y;
+                }
+
+                if (maxPos.y > maxY)
+                {
+                    maxY = maxPos.y;
+                }
+
+                if (minPos.z < minZ)
+                {
+                    minZ = minPos.z;
+                }
+
+                if (maxPos.z > maxZ)
+                {
+                    maxZ = maxPos.z;
+                }
+            }
+
+            Vector3 center = new Vector3((minX + maxX) / 2, (minY + maxY) / 2, (minZ + maxZ) / 2);
+            Vector3 size = new Vector3(maxX - minX, maxY - minY, maxZ - minZ);
+            var bounds = new Bounds(center, size);
+            return bounds;
+        }
+
+        public static Bounds Encapsulate<T>(this IEnumerable<T> tList, Func<T, Bounds> getBounds)
+        {
+            float minX = float.MaxValue;
+            float maxX = float.MinValue;
+
+            float minY = float.MaxValue;
+            float maxY = float.MinValue;
+
+            float minZ = float.MaxValue;
+            float maxZ = float.MinValue;
+            foreach (var t in tList)
+            {
+                var bonds = getBounds.Invoke(t);
+                var minPos = bonds.min;
+                var maxPos = bonds.max;
+                if (minPos.x < minX)
+                {
+                    minX = minPos.x;
+                }
+
+                if (maxPos.x > maxX)
+                {
+                    maxX = maxPos.x;
+                }
+
+                if (minPos.y < minY)
+                {
+                    minY = minPos.y;
+                }
+
+                if (maxPos.y > maxY)
+                {
+                    maxY = maxPos.y;
+                }
+
+                if (minPos.z < minZ)
+                {
+                    minZ = minPos.z;
+                }
+
+                if (maxPos.z > maxZ)
+                {
+                    maxZ = maxPos.z;
+                }
+            }
+
+            Vector3 center = new Vector3((minX + maxX) / 2, (minY + maxY) / 2, (minZ + maxZ) / 2);
+            Vector3 size = new Vector3(maxX - minX, maxY - minY, maxZ - minZ);
+            var bounds = new Bounds(center, size);
+            return bounds;
         }
     }
 }

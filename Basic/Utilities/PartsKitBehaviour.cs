3802ff9be086dcecd4b39c064045f2b26a3d6819
urochorda <2268716706@qq.com>
2025-05-05 16:30:05 +0800

    [feat]完善功能

--- Basic/Utilities/PartsKitBehaviour.cs
+++ Basic/Utilities/PartsKitBehaviour.cs
@@ -31,4 +31,94 @@ namespace PartsKit
         protected abstract void OnInit();
         protected abstract void OnDeInit();
     }
+
+    public abstract class PartsKitBehaviour<T> : MonoBehaviour
+    {
+        public bool IsInit { get; private set; }
+
+        public void Init(T t)
+        {
+            if (IsInit)
+            {
+                return;
+            }
+
+            IsInit = true;
+            OnInit(t);
+        }
+
+        public void DeInit()
+        {
+            if (!IsInit)
+            {
+                return;
+            }
+
+            IsInit = false;
+            OnDeInit();
+        }
+
+        protected abstract void OnInit(T t);
+        protected abstract void OnDeInit();
+    }
+
+    public abstract class PartsKitBehaviour<T, T2> : MonoBehaviour
+    {
+        public bool IsInit { get; private set; }
+
+        public void Init(T t, T2 t2)
+        {
+            if (IsInit)
+            {
+                return;
+            }
+
+            IsInit = true;
+            OnInit(t, t2);
+        }
+
+        public void DeInit()
+        {
+            if (!IsInit)
+            {
+                return;
+            }
+
+            IsInit = false;
+            OnDeInit();
+        }
+
+        protected abstract void OnInit(T t, T2 t2);
+        protected abstract void OnDeInit();
+    }
+
+    public abstract class PartsKitBehaviour<T, T2, T3> : MonoBehaviour
+    {
+        public bool IsInit { get; private set; }
+
+        public void Init(T t, T2 t2, T3 t3)
+        {
+            if (IsInit)
+            {
+                return;
+            }
+
+            IsInit = true;
+            OnInit(t, t2, t3);
+        }
+
+        public void DeInit()
+        {
+            if (!IsInit)
+            {
+                return;
+            }
+
+            IsInit = false;
+            OnDeInit();
+        }
+
+        protected abstract void OnInit(T t, T2 t2, T3 t3);
+        protected abstract void OnDeInit();
+    }
 }
